aebed460b497e14aa4af9fd52161f8cef7141756
Julien Dubois <julien.dubois@gmail.com>
2017-09-25 13:41:53 +0200

    Test upgrade to Yarn 1.1.0

--- generators/generator-constants.js
+++ generators/generator-constants.js
@@ -47,8 +47,8 @@ const DOCKER_SWAGGER_EDITOR = 'swaggerapi/swagger-editor:latest';
 const DOCKER_COMPOSE_FORMAT_VERSION = '2';
 
 // version of Node, Yarn, NPM
-const NODE_VERSION = '6.11.1';
-const YARN_VERSION = '0.27.5';
+const NODE_VERSION = '6.11.3';
+const YARN_VERSION = '1.1.0';
 const NPM_VERSION = '5.3.0';
 
 // all constants used throughout all generators
